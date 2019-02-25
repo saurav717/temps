d898e6e61ce4e0bd179b572999c5c87864cdd176
saurav <es16btech11007>
2019-02-26 02:23:29 +0530

    normal BFS application on a binary tree

---
+++ BFS.cpp
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+
+struct node{
+
+	int val;
+	struct node *left;
+	struct node *right;
+};
+
+struct node *root = NULL;
+
+
+
+void insert_in_position(struct node *p, struct node *q)
+{
+	if(p->val <= q->val)
+	{
+		if(q->left == NULL)
+		{
+			q->left = p;
+		}
+
+		else
+		{ 	
+			insert_in_position(p, q->left);
+		}
+	}
+
+	else if(p->val > q->val)
+	{
+		if(q->right == NULL)
+		{
+			q->right = p;
+		}
+
+		else
+		{
+			insert_in_position(p, q->right);		
+		}
+	}
+}
+
+
+
+void insert(int num)
+{
+	struct node *p;
+	p = (struct node *)malloc(sizeof(struct node));
+
+	p->val = num;
+	p->left = NULL;
+	p->right = NULL;
+
+	if(root == NULL)
+	{
+		root = p;
+	}
+
+	else
+	{
+		insert_in_position(p, root);
+	}
+}
+
+void print(struct node *p)
+{
+	cout << p->val << " ";
+
+	if(p->left!= NULL)
+	{ 	
+		print(p->left);
+	}
+
+	if(p->right!=NULL)
+	{ 
+		print(p->right);
+	}
+
+}
+
+void BFS(struct node *p)
+{
+
+	queue<struct node *> level;
+
+	level.push(p);
+	//cout << "queue size = " << level.size();
+
+	while (!level.empty())
+	{
+		struct node *q = level.front();
+		
+		cout << q->val << " ";
+		
+		if(q->left!=NULL)
+		{
+			level.push(q->left);
+		}
+
+		if(q->right != NULL)
+		{
+			level.push(q->right);
+		}
+
+		level.pop();
+	}
+
+}
+
+
+int main()
+{
+
+	int num;
+
+	insert(1);
+	insert(2);
+	insert(3);
+	insert(4);
+	insert(5);
+	insert(6);
+	insert(7);
+
+	cout << "In-order printing for reference " << endl;
+
+
+	print(root);
+
+	cout << endl;
+
+	cout << "using BFS" << endl;
+
+	BFS(root);
+
+}
